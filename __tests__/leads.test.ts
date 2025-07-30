f8504d4074aff65b788dab83471a722332a4bbc3
Vincent Taboada <vftaboada@gmail.com>
2025-07-30 10:21:06 +0800

    add tests

--- __tests__/leads.test.ts
+++ __tests__/leads.test.ts
@@ -15,159 +15,157 @@ beforeAll(async () => {
   request = Request(app);
 });
 
-afterAll(async () => {
-  // Optionally, close DB connection here
-  await import('../src/dataSource').then(({ AppDataSource }) => AppDataSource.destroy());
-});
-
 describe('LeadResolver', () => {
-  it('should register a lead', async () => {
-    const response = await request 
-      .post('/graphql')
-      .send({
-        query: `
-          mutation Register($input: RegisterInput!) {
-            register(input: $input) {
-              id
-              name
-              email
-              mobile
-              postcode
-              services {
+  describe('Register Lead', () => {
+    it('should register a lead', async () => {
+      const response = await request 
+        .post('/graphql')
+        .send({
+          query: `
+            mutation Register($input: RegisterInput!) {
+              register(input: $input) {
                 id
                 name
+                email
+                mobile
+                postcode
+                services {
+                  id
+                  name
+                }
               }
             }
-          }
-        `,
-        variables: {
-          input: {
-            name: 'John Doe',
-            email: 'john@example.com',
-            mobile: '1234567890',
-            postcode: '12345',
-            services: [1],
+          `,
+          variables: {
+            input: {
+              name: 'John Doe',
+              email: 'john@example.com',
+              mobile: '1234567890',
+              postcode: '12345',
+              services: ["delivery"],
+            },
           },
-        },
-      })
-      .expect(200);
+        })
 
-    expect(response.body.data.register).toHaveProperty('id');
-    expect(response.body.data.register.name).toBe('John Doe');
-    expect(response.body.data.register.email).toBe('john@example.com');
-    expect(response.body.data.register.mobile).toBe('1234567890');
-    expect(response.body.data.register.postcode).toBe('12345');
-    expect(response.body.data.register.services).toHaveLength(1);
-    expect(response.body.data.register.services[0]).toHaveProperty('id', service.id);
-    expect(response.body.data.register.services[0]).toHaveProperty('name', service.name);
-  });
+      expect(response.status).toBe(200);
+      expect(response.body.data.register).toHaveProperty('id');
+      expect(response.body.data.register.name).toBe('John Doe');
+      expect(response.body.data.register.email).toBe('john@example.com');
+      expect(response.body.data.register.mobile).toBe('1234567890');
+      expect(response.body.data.register.postcode).toBe('12345');
+      expect(response.body.data.register.services).toHaveLength(1);
+      expect(response.body.data.register.services[0]).toHaveProperty('id', 1);
+      expect(response.body.data.register.services[0]).toHaveProperty('name', "delivery");
+    });
+  })
 
-  it('should fetch all leads', async () => {
-    const response = await request
-      .post('/graphql')
-      .send({
-        query: `
-          query {
-            leads {
-              id
-              name
-              email
-              mobile
-              postcode
-              services {
+  describe("Fetch Leads", () => {
+    it('should fetch all leads', async () => {
+      const response = await request
+        .post('/graphql')
+        .send({
+          query: `
+            query {
+              leads {
                 id
                 name
+                email
+                mobile
+                postcode
+                services {
+                  id
+                  name
+                }
               }
             }
-          }
-        `,
-      })
-      .expect(200);
+          `,
+        })
+        .expect(200);
 
-    expect(response.body.data.leads).toBeInstanceOf(Array);
-    expect(response.body.data.leads.length).toBeGreaterThan(0);
-    response.body.data.leads.forEach((lead: any) => {
-      expect(lead).toHaveProperty('id');
-      expect(lead).toHaveProperty('name');
-      expect(lead).toHaveProperty('email');
-      expect(lead).toHaveProperty('mobile');
-      expect(lead).toHaveProperty('postcode');
-      expect(lead.services).toBeInstanceOf(Array);
+      expect(response.body.data.leads).toBeInstanceOf(Array);
+      expect(response.body.data.leads.length).toBeGreaterThan(0);
+      response.body.data.leads.forEach((lead: any) => {
+        expect(lead).toHaveProperty('id');
+        expect(lead).toHaveProperty('name');
+        expect(lead).toHaveProperty('email');
+        expect(lead).toHaveProperty('mobile');
+        expect(lead).toHaveProperty('postcode');
+        expect(lead.services).toBeInstanceOf(Array);
+      });
     });
   });
-});
 
-describe('LeadResolver - Single Lead', () => {
-  it('should fetch a lead by ID', async () => {
-    const service = await AppDataSource.getRepository(Service).save({ name: 'delivery' });
-    const lead = await AppDataSource.getRepository(Lead).save({
-      name: 'Jane Doe',
-      email: 'jane@example.com',
-      mobile: '0987654321',
-      postcode: '54321',
-      services: [service],
+  describe('Fetch Single Lead', () => {
+    it('should fetch a lead by ID', async () => {
+      const lead = await AppDataSource.getRepository(Lead).save({
+        name: 'Jane Doe',
+        email: 'jane@example.com',
+        mobile: '0987654321',
+        postcode: '54321',
+        services: [{id: 1}],
+      });
+
+      const response = await request 
+        .post('/graphql')
+        .send({
+          query: `
+            query GetLead($id: Int!) {
+              lead(id: $id) {
+                id
+                name
+                email
+                mobile
+                postcode
+                services {
+                  id
+                  name
+                }
+              }
+            }
+          `,
+          variables: {
+            id: lead.id,
+          },
+        })
+
+      expect(response.status).toBe(200);
+      expect(response.body.data.lead).toHaveProperty('id', lead.id);
+      expect(response.body.data.lead.name).toBe('Jane Doe');
+      expect(response.body.data.lead.email).toBe('jane@example.com');
+      expect(response.body.data.lead.mobile).toBe('0987654321');
+      expect(response.body.data.lead.postcode).toBe('54321');
+      expect(response.body.data.lead.services).toHaveLength(1);
+      expect(response.body.data.lead.services[0]).toHaveProperty('id', 1);
+      expect(response.body.data.lead.services[0]).toHaveProperty('name', 'delivery');
     });
 
-    const response = await request 
-      .post('/graphql')
-      .send({
-        query: `
-          query GetLead($id: ID!) {
-            lead(id: $id) {
-              id
-              name
-              email
-              mobile
-              postcode
-              services {
+    it('should return null for a non-existing lead', async () => {
+      const response = await request
+        .post('/graphql')
+        .send({
+          query: `
+            query GetLead($id: Int!) {
+              lead(id: $id) {
                 id
                 name
+                email
+                mobile
+                postcode
+                services {
+                  id
+                  name
+                }
               }
             }
-          }
-        `,
-        variables: {
-          id: lead.id,
-        },
-      })
-      .expect(200);
+          `,
+          variables: {
+            id: 9999, // Assuming this ID does not exist
+          },
+        })
 
-    expect(response.body.data.lead).toHaveProperty('id', lead.id);
-    expect(response.body.data.lead.name).toBe('Jane Doe');
-    expect(response.body.data.lead.email).toBe('jane@example.com');
-    expect(response.body.data.lead.mobile).toBe('0987654321');
-    expect(response.body.data.lead.postcode).toBe('54321');
-    expect(response.body.data.lead.services).toHaveLength(1);
-    expect(response.body.data.lead.services[0]).toHaveProperty('id', service.id);
-    expect(response.body.data.lead.services[0]).toHaveProperty('name', service.name);
-  });
-
-  it('should return null for a non-existing lead', async () => {
-    const response = await request
-      .post('/graphql')
-      .send({
-        query: `
-          query GetLead($id: ID!) {
-            lead(id: $id) {
-              id
-              name
-              email
-              mobile
-              postcode
-              services {
-                id
-                name
-              }
-            }
-          }
-        `,
-        variables: {
-          id: 9999, // Assuming this ID does not exist
-        },
-      })
-      .expect(200);
-
-    expect(response.body.data.lead).toBeNull();
+      expect(response.status).toBe(200);
+      expect(response.body.data.lead).toBeNull();
+    });
   });
 });
 
